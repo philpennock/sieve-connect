d04d21eaf8a798e262f3fa6f2055aa0f962207b4
Phil Pennock <codehacks@spodhuis.org>
2010-04-29 00:13:32 +0000

    Fix copyright year.

--- sieve-connect.pl
+++ sieve-connect.pl
@@ -4,7 +4,7 @@
 #
 # timsieved client script
 #
-# Copyright © 2006-2009 Phil Pennock.  All rights reserved.
+# Copyright © 2006-2010 Phil Pennock.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
