1ef355448d3f3d216956b8561777b727085397d3
Phil Pennock <pdp@spodhuis.org>
2016-08-29 17:05:46 -0400

    Update copyright year

--- sieve-connect.pre.pl
+++ sieve-connect.pre.pl
@@ -2,7 +2,7 @@
 #
 # MANAGESIEVE (timsieved) client script
 #
-# Copyright © 2006-2014 Phil Pennock.  All rights reserved.
+# Copyright © 2006-2016 Phil Pennock.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
